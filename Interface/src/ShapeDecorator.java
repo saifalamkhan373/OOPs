67ab8b6da73caedf89c20b39a833054f3e0ccc2b
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-08-23 23:39:29 +0530

    decorative design pattern

---
+++ Interface/src/ShapeDecorator.java
@@ -0,0 +1,11 @@
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decorateShape;
+    public ShapeDecorator (Shape decorateShape){
+        this.decorateShape=decorateShape;
+    }
+
+    @Override
+    public void draw() {
+        decorateShape.draw();
+    }
+}
