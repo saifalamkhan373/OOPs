fad040afdd06391fbde6770b08a01ded87f77240
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-08-31 12:57:52 +0530

    new examples for class and obejct

--- Class and Object/src/Main.java
+++ Class and Object/src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     int num = 5;
 
-    Main() {
+    private Main() {
+        int a=2, b=3;
+        int sum = a+b;
         System.out.println("Constructor is being called");
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
         Main obj = new Main();
-        System.out.println(obj.num);
+//        System.out.println(obj.num);
     }
 }
