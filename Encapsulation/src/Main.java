cca263cb2f1344c2c97f4c8761eaaac44eafc039
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-09-04 17:02:40 +0530

    Example of Encapsulation

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        person obj=new person();
+        obj.setName("Saif Alam Khan");
+        System.out.println(obj.getName());
+
+        System.out.println("Hello world!");
+    }
+}
